dfe9666a206f2fb8ffac39316ea477faaed90e91
dalan <dalan_123@126.com>
2025-03-25 22:37:36 +0800

    添加线性神经网络实现示例 - 生成数据集 - 将生成的数据集打乱，并以小批量的形式返回 - 初始化模型参数 - 定义模型 - 定义损失函数 - 定义优化算法 - 训练模型

--- src/bin/basic_model_demos.rs
+++ src/bin/basic_model_demos.rs
@@ -1,6 +1,9 @@
-use candle_core::{DType, Device, Module, Result, Tensor};
-use plotters::prelude::{BitMapBackend, ChartBuilder, Color, DiscreteRanged, IntoDrawingArea, IntoLinspace, IntoSegmentedCoord, LabelAreaPosition, LineSeries, PathElement, BLACK, BLUE, GREEN, RED, WHITE};
+use std::ops::Div;
+use candle_core::{DType, Device, IndexOp, Module, Result, Tensor, Var};
+use candle_core::backprop::GradStore;
+use plotters::prelude::{BitMapBackend, ChartBuilder, Circle, Color, DiscreteRanged, EmptyElement, IntoDrawingArea, IntoFont, IntoLinspace, IntoSegmentedCoord, LabelAreaPosition, LineSeries, PathElement, PointSeries, Text, BLACK, BLUE, GREEN, RED, WHITE};
 use plotters::prelude::full_palette::PURPLE;
+use rand::prelude::SliceRandom;
 
 /// 结合plotters绘制正态分布图
 ///
@@ -204,9 +207,170 @@ fn candle_basic_modle_linermodel_demo_v3(device: &Device) -> Result<()> {
     Ok(())
 }
 
+/// 根据构建的数据集绘制散点图
+fn dataset_noise_draw_points_demo(features: &Tensor, labels: &Tensor) -> Result<()> {
+    const OUT_FILE_NAME: &str = "basic-normal-dist.png";
+    let root = BitMapBackend::new(OUT_FILE_NAME, (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let root_area = root
+        .titled("线性模型", ("sans-serif", 20)).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_area)
+        .margin(5)
+        .caption("散点图", ("sans-serif", 20))
+        .set_label_area_size(LabelAreaPosition::Top, 60)
+        .set_label_area_size(LabelAreaPosition::Left, 60)
+        .set_label_area_size(LabelAreaPosition::Bottom, 60)
+        .set_label_area_size(LabelAreaPosition::Right, 60)
+        .build_cartesian_2d(-4.0f32..4.0f32,-10.0f32..20.0).unwrap()
+        ;
+
+    chart.configure_mesh()
+        .x_labels(10)
+        .y_labels(10)
+        .y_label_formatter(&|y| format!("{:.1}f", *y))
+        .draw().unwrap();
+
+    chart.draw_series(PointSeries::<_, _, Circle<_,_>, _>::new(
+        // 特征和标签
+        // 基于zip合并，构建元组(feature, label)
+        (&features.i((..,1))?).to_vec1::<f32>()?.into_iter()
+            .zip((&labels.i((..,0))?).to_vec1::<f32>()?.into_iter())
+            // .enumerate()
+            // .map(|(i, (feature, label))| {
+            //     // println!("index={i}, feature={feature}, label={label}");
+            //     (feature, label)
+            // })
+            .map(|(feature, label)| {
+                // println!("feature={feature}, label={label}");
+                (feature, label)
+            })
+            .collect::<Vec<_>>()
+        // 先获取特征数据，再根据索引获取标签
+        // (&features.i((..,1))?).to_vec1::<f32>()?.into_iter()
+        //     .enumerate()
+        //     .map(|(i, feature)| {
+        //        let binding = (&labels.i((..,0)).unwrap()).to_vec1::<f32>().unwrap();
+        //        let label= binding.get(i).unwrap().to_owned();
+        //        // println!("feature={feature}, label={label}");
+        //        (feature, label)
+        //     })
+        ,2
+        , GREEN.filled(),
+    )
+    ).unwrap()
+    ;
+    // To avoid the IO failure being ignored silently, we manually call the present function
+    root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+    println!("Result has been saved to {}", OUT_FILE_NAME);
+    Ok(())
+}
+/// 根据带有噪声的线性来构建数据集
+/// y = Xw + b + e
+fn candle_baisc_modle_dataset_noise_demo(device: &Device) -> Result<()> {
+    // 线性模型
+    let true_w = Tensor::new(&[2f32,3.4], device)?;
+    let true_b = 4.2f32;
+
+    /// step-1: 生成数据集
+    fn synthetic_data(w: &Tensor, b: f32, num_examples: usize, device: &Device) -> Result<(Tensor, Tensor)> {
+        let X = Tensor::randn(0f32, 1.0f32, (num_examples, w.dim(0)?), &device)?;
+        let y = (X.matmul(&w)? + b as f64)?;
+        let y_clone = y.clone();
+        let y = (y + Tensor::randn(0f32, 1.0f32, y_clone.shape(), &device)?)?;
+
+        Ok( (X, y.reshape(((),1))?))
+    }
+
+    let (features, labels) = synthetic_data(&true_w.reshape(((),1))?, true_b, 1000, device)?;
+    println!("features={}, lables={}", features.i(0)?, labels.i(0)?);
+    // 绘制散点图
+    // dataset_noise_draw_points_demo(&features, &labels);
+    /// step-2 将生成的数据集打乱，并以小批量的形式返回
+    /// batch_size 小批量
+    /// features 特征数据
+    /// labels 标签数据
+    fn data_iter(batch_size: usize, features: &Tensor, labels: &Tensor, device: &Device) -> Result<Vec<(Tensor, Tensor)>>{
+        let num_examples = features.dim(0)?;
+        let mut indices = Tensor::arange(0, num_examples as u32, &device)?.to_vec1::<u32>()?;
+        let mut rng = rand::thread_rng();
+        indices.shuffle(&mut rng);
+
+        let mut batch_examples = Vec::<(Tensor, Tensor)>::new();
+
+        for (i, batch_indices) in indices.chunks(batch_size).enumerate() {
+            let batch_indices = Tensor::from_slice(batch_indices, (batch_size,), &device)?;
+            let batch_features = features.i(&batch_indices)?;
+            let batch_labels = labels.i(&batch_indices)?;
+            // println!("batch_index={i}, batch_features={batch_features}, batch_labels={batch_labels}");
+            //
+            batch_examples.push((batch_features, batch_labels));
+        }
+
+        Ok(batch_examples)
+    }
+    let batch_size = 10;
+    // data_iter(batch_size, &features, &labels, device);
+    /// step-3 初始化模型参数 由于需要grad，则需要Var来定义
+    let w = Var::randn(0f32, 0.01f32, (2,1), &device)?;
+    let b = Var::zeros(1, DType::F32, &device)?;
+    /// step-4 定义模型
+    fn linreg(X: &Tensor, w: &Tensor, b: &Tensor) -> Result<Tensor> {
+        // X * w + b
+        let y = (X.matmul(w)?.broadcast_add(b))?;
+        Ok(y)
+    }
+
+    /// step-5 定义损失函数
+    fn squared_loss(y_hat: &Tensor, y: &Tensor) -> Result<Tensor> {
+        // (y_hat - y)^2/2
+        let l = (y_hat - y.reshape(y_hat.shape()))?.sqr()?/2f64;
+        l
+    }
+
+    /// step-6 定义优化算法
+    fn sgd(gradstore: GradStore, params: &[&Var], lr: f32, batch_size: usize) -> Result<()> {
+        for param in params {
+            let new_param = (param.as_tensor() - ((lr as f64) * gradstore.get(param.as_tensor()).expect(format!("no grad for {}", param).as_ref()))? / batch_size as f64)?;
+            param.set(&new_param);
+            // param.set(((param.as_tensor() - ((lr as f64) * param.as_tensor())? / batch_size as f64)?).as_ref());
+            // w -= lr * w.grad() / batch_size
+        }
+        Ok(())
+    }
+
+    /// step-7 训练模型
+    let lr = 0.03;
+    let num_epochs = 3;
+    let net = linreg;
+    let loss = squared_loss;
+    for epoch in 0..num_epochs {
+        for (batch_i, (X, y)) in data_iter(batch_size, &features, &labels, &device)?.into_iter().enumerate() {
+            // 前向传播
+            let y_hat = net(&X, &w, &b)?;
+            // 计算损失
+            let l = loss(&y_hat, &y)?;
+            // 反向传播
+            let gradstore = l.backward()?;
+            // 优化模型
+            sgd(gradstore, &[&w, &b], lr, batch_size)?;
+        }
+        // 测试
+        let train_1 = loss(&net(&features, &w, &b)?, &labels)?;
+        println!("epoch {:?}, loss {:.2}", epoch+1, train_1.mean(0)?);
+        // 通过训练得到相对较优的参数w和b
+        println!("w={}, b={}", w.as_tensor(), b.as_tensor());
+        println!("w的估计误差={}, b的估计误差={}", (&true_w.reshape(w.shape())?-w.as_tensor())?, (Tensor::new(&[true_b], device)?.reshape(b.shape())-b.as_tensor())?)
+    }
+
+    Ok(())
+}
+
+
 fn main() {
     let device = Device::Cpu;
-    candle_basic_modle_linermodel_demo_v3(&device);
+    candle_baisc_modle_dataset_noise_demo(&device);
+    // candle_basic_modle_linermodel_demo_v3(&device);
     // candle_basic_modle_linermodel_demo_v2(&device);
     // candle_basic_modle_linermodel_demo_v1(&device);
     // candle_basic_model_demo(&device);
